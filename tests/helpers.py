52c0b877008301c5024a16659f81fb96892515f9
Mythir <larsvanleeuwen94@gmail.com>
2024-09-11 12:28:14 +0200

    Add test for provider

---
+++ tests/helpers.py
@@ -0,0 +1,28 @@
+from compute_api_client import BackendStatus, BackendType
+
+
+def create_backend_type(
+    gateset: list[str] = [],
+    topology: list[list[int]] = [],
+    nqubits: int = 0,
+    default_number_of_shots: int = 1024,
+    max_number_of_shots: int = 2048,
+    name: str = "qi_backend",
+) -> BackendType:
+    """Helper for creating a backendtype with only the fields you care about."""
+    return BackendType(
+        name=name,
+        nqubits=nqubits,
+        gateset=gateset,
+        topology=topology,
+        id=1,
+        is_hardware=True,
+        image_id="qi_backend",
+        features=[],
+        default_compiler_config="",
+        status=BackendStatus.IDLE,
+        default_number_of_shots=default_number_of_shots,
+        max_number_of_shots=max_number_of_shots,
+        infrastructure="QCI",
+        description="A Quantum Inspire backend",
+    )
--- tests/test_qi_backend.py
+++ tests/test_qi_backend.py
@@ -1,36 +1,9 @@
 import logging
 
 import pytest
-from compute_api_client import BackendStatus, BackendType
 
 from qiskit_quantuminspire.qi_backend import QIBackend
-
-
-def create_backend_type(
-    gateset: list[str] = [],
-    topology: list[list[int]] = [],
-    nqubits: int = 0,
-    default_number_of_shots: int = 1024,
-    max_number_of_shots: int = 2048,
-    name: str = "qi_backend",
-) -> BackendType:
-    """Helper for creating a backendtype with only the fields you care about."""
-    return BackendType(
-        name=name,
-        nqubits=nqubits,
-        gateset=gateset,
-        topology=topology,
-        id=1,
-        is_hardware=True,
-        image_id="qi_backend",
-        features=[],
-        default_compiler_config="",
-        status=BackendStatus.IDLE,
-        default_number_of_shots=default_number_of_shots,
-        max_number_of_shots=max_number_of_shots,
-        infrastructure="QCI",
-        description="A Quantum Inspire backend",
-    )
+from tests.helpers import create_backend_type
 
 
 @pytest.mark.parametrize(
