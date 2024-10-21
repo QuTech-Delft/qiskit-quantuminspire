76bffe90e2cf5846f64853ae8600652470392d12
napocornejo <55136242+napocornejo@users.noreply.github.com>
2024-10-21 16:56:27 +0200

    [QI2-1119] Support running in an existing event loop (i.e. jupyter notebook) (#36)
    Co-authored-by: napocornejo <ncornejo@geokapti.nl>

--- qiskit_quantuminspire/utils.py
+++ qiskit_quantuminspire/utils.py
@@ -1,3 +1,7 @@
+import asyncio
+import concurrent
+from typing import Any, Coroutine
+
 from qiskit.transpiler import CouplingMap
 
 
@@ -10,3 +14,12 @@ def is_coupling_map_complete(coupling_map: CouplingMap) -> bool:
     is_semicomplete = all(distance in [1, 0] for distance in distance_matrix.flatten())
 
     return is_semicomplete and coupling_map.is_symmetric
+
+
+def run_async(async_function: Coroutine[Any, Any, Any]) -> Any:
+    try:
+        _ = asyncio.get_running_loop()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            return executor.submit(asyncio.run, async_function).result()
+    except RuntimeError:
+        return asyncio.run(async_function)
