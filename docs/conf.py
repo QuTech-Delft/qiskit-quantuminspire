903a8f5cb94fd834e213fcb265888aa42c514df2
Mythir <larsvanleeuwen94@gmail.com>
2024-11-04 09:13:01 +0100

    Updated theme

--- docs/conf.py
+++ docs/conf.py
@@ -56,6 +56,7 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "qiskit_sphinx_theme",
     "myst_nb",
 ]
 
@@ -141,7 +142,7 @@ nb_execution_mode = "off"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "furo"
+html_theme = "qiskit-ecosystem"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
