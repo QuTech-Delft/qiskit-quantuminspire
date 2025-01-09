b873e7f63861d5ead7ccdfa564fa900db6fe9b67
Mythir <larsvanleeuwen94@gmail.com>
2025-01-09 17:03:02 +0100

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -64,10 +64,7 @@ extensions = [
 templates_path = ["_templates"]
 
 # The suffix of source filenames.
-# source_suffix = {
-#     ".rst": "restructuredtext",
-#     ".md": "markdown",
-# }
+# source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
 
 # The encoding of source files.
 # source_encoding = 'utf-8-sig'
--- docs/index.rst
+++ docs/index.rst
@@ -9,6 +9,7 @@ Welcome to the documentation of **Qiskit-QuantumInspire**, a Qiskit plugin for t
 
    Documentation Home <index>
    Getting Started <getting_started/index>
+   Hybrid Computing <hybrid/index>
    Example Notebooks <notebooks/index>
    Contributing and Development <contributing>
    License <license>
