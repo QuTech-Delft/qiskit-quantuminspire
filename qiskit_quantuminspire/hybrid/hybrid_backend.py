16e2a59e7c462e4ad2a0553a8ed6c435714705e5
Mythir <larsvanleeuwen94@gmail.com>
2025-01-09 11:46:25 +0100

    Return a job

--- qiskit_quantuminspire/hybrid/hybrid_backend.py
+++ qiskit_quantuminspire/hybrid/hybrid_backend.py
@@ -35,3 +35,4 @@ class QIHybridBackend(QIBaseBackend):
     def run(self, run_input: Union[QuantumCircuit, List[QuantumCircuit]], **options: Any) -> QIHybridJob:
         job = QIHybridJob(run_input=run_input, backend=self, quantum_interface=self._quantum_interface, **options)
         job.submit()
+        return job
