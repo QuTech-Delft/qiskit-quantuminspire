c2c575f15d997ad258c872aa01720c642f3a5b9e
Mythir <larsvanleeuwen94@gmail.com>
2025-01-21 20:39:08 +0100

    Support barrier in Qiskit-QI

--- qiskit_quantuminspire/cqasm.py
+++ qiskit_quantuminspire/cqasm.py
@@ -1,7 +1,10 @@
+from typing import Any
+
 from opensquirrel import CircuitBuilder
 from opensquirrel.ir import Bit, Qubit
 from opensquirrel.writer import writer
 from qiskit import QuantumCircuit
+from qiskit.circuit import CircuitInstruction
 
 _QISKIT_TO_OPENSQUIRREL_MAPPING: dict[str, str] = {
     "id": "I",
@@ -27,35 +30,45 @@ _QISKIT_TO_OPENSQUIRREL_MAPPING: dict[str, str] = {
 }
 
 
+def _add_instruction(builder: CircuitBuilder, circuit_instruction: Any) -> None:
+    operation = circuit_instruction.operation
+    name = operation.name
+    params = [param for param in operation.params]
+    qubit_operands = [Qubit(qubit._index) for qubit in circuit_instruction.qubits]
+    clbit_operands = [Bit(clbit._index) for clbit in circuit_instruction.clbits]
+
+    try:
+        # Get the gate's method in the CircuitBuilder class, call with operands
+        # All of the builder's methods follow the same pattern, first the qubit operands, then parameters
+        # Only method with classical bit operands is measure, which does not have parameters
+        getattr(builder, _QISKIT_TO_OPENSQUIRREL_MAPPING[name])(*qubit_operands, *clbit_operands, *params)
+    except KeyError:
+        raise NotImplementedError(
+            f"Unsupported instruction: {name}. Please edit your circuit or use Qiskit transpilation to support "
+            + "your selected backend."
+        )
+
+
 def dumps(circuit: QuantumCircuit) -> str:
     """Return the cQASM representation of the circuit."""
     builder = CircuitBuilder(circuit.num_qubits, circuit.num_clbits)
     for circuit_instruction in circuit.data:
-        print(circuit_instruction)
         operation = circuit_instruction.operation
         name = operation.name
-        params = [param for param in operation.params]
-        qubit_operands = [Qubit(qubit._index) for qubit in circuit_instruction.qubits]
-        clbit_operands = [Bit(clbit._index) for clbit in circuit_instruction.clbits]
-
-        # TODO: Handler barrier per qubit?
 
         if name == "delay":
             if circuit_instruction.unit != "dt":
-                raise ValueError(
+                raise NotImplementedError(
                     f"Unsupported delay unit {circuit_instruction.unit} in: {circuit_instruction}. Only 'dt' is supported."
                 )
 
-        try:
-            # Get the gate's method in the CircuitBuilder class, call with operands
-            # All of the builder's methods follow the same pattern, first the qubit operands, then parameters
-            # Only method with classical bit operands is measure, which does not have parameters
-            getattr(builder, _QISKIT_TO_OPENSQUIRREL_MAPPING[name])(*qubit_operands, *clbit_operands, *params)
-        except KeyError:
-            raise NotImplementedError(
-                f"Unsupported instruction: {name}. Please edit your circuit or use Qiskit transpilation to support "
-                + "your selected backend."
-            )
+        if name == "barrier":
+            # Opensquirrel does not support multi-qubit barriers.
+            for qubit in circuit_instruction.qubits:
+                _add_instruction(builder, CircuitInstruction(operation=operation, qubits=[qubit]))
+            continue
+
+        _add_instruction(builder, circuit_instruction)
 
     cqasm: str = writer.circuit_to_string(builder.to_circuit())
 
