6b5a6a804ce1bd721a4251e16f7edae50ebd31a7
Mythir <larsvanleeuwen94@gmail.com>
2025-03-14 12:07:53 +0100

    Fix exception handling in run_async()

--- qiskit_quantuminspire/utils.py
+++ qiskit_quantuminspire/utils.py
@@ -19,7 +19,8 @@ def is_coupling_map_complete(coupling_map: CouplingMap) -> bool:
 def run_async(async_function: Coroutine[Any, Any, Any]) -> Any:
     try:
         _ = asyncio.get_running_loop()
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            return executor.submit(asyncio.run, async_function).result()
     except RuntimeError:
         return asyncio.run(async_function)
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        return executor.submit(asyncio.run, async_function).result()
