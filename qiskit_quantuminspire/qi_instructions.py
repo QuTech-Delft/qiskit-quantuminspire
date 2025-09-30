6c1a73cf09dcb702b7cabd7d1b054c25294fa5b1
Nischal Sehrawat <nischal.sehrawat@tno.nl>
2025-09-30 16:39:09 +0200

    Updated docs

--- qiskit_quantuminspire/qi_instructions.py
+++ qiskit_quantuminspire/qi_instructions.py
@@ -5,7 +5,6 @@ class Asm(Instruction):  # type: ignore[misc]
     # Mark as directive like Barrier,
     # see https://docs.quantum.ibm.com/api/qiskit/circuit_library#standard-directives
     _directive = True
-    NAME = "asm"
 
     def __init__(self, backend_name: str = "", asm_code: str = ""):
-        super().__init__(self.NAME, 0, 0, [backend_name, asm_code])
+        super().__init__("asm", 0, 0, [backend_name, asm_code])
